848f7d69e51529aa3f6c59fb48775bdd157178ac
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-04-20 23:00:01 +0900

    Add navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def nav_link_to(name, path)
+    active = request.path == path
+    nav_item_html_class = active ? 'nav-item active' : 'nav-item'
+    content_tag(:li, class: nav_item_html_class) do
+      link_to path, class: 'nav-link' do
+        if active
+          h(name) + content_tag(:span, '(current)', class: 'sr-only')
+        else
+          name
+        end
+      end
+    end
+  end
 end
