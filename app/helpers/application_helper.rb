90c56aa6df842b3aa71dad600785bdc7824cd7ff
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-04-20 23:59:04 +0900

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = I18n.t(:'nav.site_name')
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
   def nav_link_to(name, path)
     active = request.path == path
     nav_item_html_class = active ? 'nav-item active' : 'nav-item'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select 'title', I18n.t(:'nav.site_name')
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select 'title', full_title(I18n.t(:'nav.about'))
   end
 
 end
