e9f16d1ddc5a4a03f3d67662fd6e74a27d35852a
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-04-21 00:12:32 +0900

    Change path

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select 'title', I18n.t(:'nav.site_name')
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select 'title', full_title(I18n.t(:'nav.about'))
   end
