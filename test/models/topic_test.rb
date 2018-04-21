ea2efa593f5a42b668b995de24ab30f1bf87ad0f
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-04-21 10:50:21 +0900

    Add test for most recent topic

--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class TopicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "most recent" do
+    one = topics(:one)
+    two = topics(:two)
+    one.body += ' (modified)'
+    one.save!
+    assert_equal one, Topic.most_recent
+    two.body += ' (modified)'
+    two.save!
+    assert_equal two, Topic.most_recent
+    two.delete_flg = true
+    two.save!
+    assert_equal one, Topic.most_recent
+  end
 end
