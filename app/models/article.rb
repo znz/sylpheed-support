8cdebb38e7c6abcc2fa7ceaa624864f9e4a35c8a
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-04-21 12:11:26 +0900

    Add validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,13 @@
 class Article < ApplicationRecord
   acts_as_nested_set counter_cache: :children_count
 
+  validates :remote_addr, presence: true
+  validates :name, :subject, :body, presence: true
+  validates :name, length: { maximum: 30 }
+  validates :subject, length: { maximum: 50 }
+  validates :body, length: { maximum: 2000 }
+  validates :mail_addr, length: { maximum: 100 }
+  validates :url, length: { maximum: 100 }
+
   THRESHOLD_HOURS = 24
 end
